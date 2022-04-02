d0c8f0b7a9d0566488d237b9144d158c7f84889f
Mehmet <mathdenizi@gmail.com>
2022-04-02 20:34:51 +0200

    First review for Data Structure

---
+++ 01-Arrays/src/main/java/TwoSum.java
@@ -0,0 +1,71 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] myarray={2,7,13,15,16,18,20,21,25,27};
+        System.out.println(Arrays.toString(twoSum1(myarray,42)));
+        System.out.println("Optimal Solution .....");
+        System.out.println(Arrays.toString(twoSum2(myarray,42)));
+        System.out.println("Two Pointer Solution .....");
+        System.out.println(Arrays.toString(mySolution(myarray,42)));
+    }
+    public static int[] twoSum1(int[] array, int target){
+        for(int i=0;i<array.length-1;i++){  //n
+            for(int j=i+1; j<array.length;j++) //n
+                if((array[i]+array[j])==target) return new int[]{i,j};//c
+        }
+        return new int[]{};
+    }
+    // my Optimal Solution O(n)
+    public static int[] twoSum2(int[] array, int target){
+
+
+        //Create HashMap
+        Map<Integer,Integer> map=new HashMap<>();
+        Arrays.sort(array);
+        //Iterate over the array.
+        // If a potential match exists return that indices else put the array value and index to map
+        for(int i=0;i< array.length;i++){
+            int potentialMatch=target-array[i];
+            if(map.containsKey(potentialMatch)) return new int[]{i, map.get(potentialMatch)};
+            else map.put(array[i],i);
+        }
+        return new int[]{};
+    }
+    public static int[] twoSumTwoPointer(int[] array, int target) {
+        //Sort the array. (This has a cost! O(nlogn))
+        Arrays.sort(array);
+        //Define two index pointers i and j (i from 0 to length. / j from length to 0)
+        int i=0;
+        int j=array.length-1;
+        while (i < j) {
+            int sum= array[i]+array[j];
+            if(sum==target) return new int[]{i,j};
+            else if (sum<target) i++;
+            else j--;
+        }
+        return new int[] {};
+    }
+
+    public static int[] mySolution(int[] array, int target){
+
+        Arrays.sort(array);
+        int i = 0;
+        int j = array.length-1;
+        while(i < j ) {
+            int sum = array[i] + array[j];
+            if(sum == target) {
+              return new int[]{i,j};
+            } else if (sum < target ) {
+                i++;
+            } else {
+                j--;
+            }
+
+        }
+
+        return new int[]{};
+    }
+}
